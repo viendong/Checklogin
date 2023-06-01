31d11fb71a4259da6df716757bd5002b82db3095
viendong <vothikieuviendong@gmail.com>
2023-06-01 13:09:03 +0700

    Update

--- src/main/java/org/example/CheckLogin.java
+++ src/main/java/org/example/CheckLogin.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 public class CheckLogin {
 
     String URL_login = "http://127.0.0.1:5500/login.html";
-    String URL_dashBoard = "http://127.0.0.1:5500/action_page.php";
+    String URL_homepage = "http://127.0.0.1:5500/action_page.html";
     String submitBtn = "submit";
 
     WebDriver driver = new ChromeDriver();
@@ -29,25 +29,77 @@ public class CheckLogin {
         System.setProperty("webdriver.chrome.driver", "/Users/kvdong/Documents/java/chromedriver_mac_arm64/chromedriver");
         WebDriverManager.chromedriver().setup();
 //        WebDriver driver = new ChromeDriver();
-        driver.navigate().to(URL_login);
+//        driver.navigate().to(URL_login);
+        driver.get(URL_login);
         driver.findElement(By.name("login")).click();
+    }
+
+    @Test
+    public void loginByAdmin() {
+//        System.out.println("print");
+        login("username_01", "pass123");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_homepage);
     }
 
     @Test
     public void loginWithBlankField() {
         login("", "");
-//        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
+    }
+
+    @Test
+    public void loginWithSpaceChar() {
+        login(" ", " ");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
+    }
+    //fail
+    @Test
+    public void loginByUsername() {
+//        System.out.println("print");
+        login("username_01", "");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
+    }
+
+    @Test
+    public void loginByUsernameSpecial() {
+//        System.out.println("print");
+        login("username_01#", "");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
     }
     @Test
-    public void loginByAdmin() {
+    public void loginByUsernameSpace() {
 //        System.out.println("print");
-        login("username_01", "pass123");
-        Assert.assertEquals(driver.getCurrentUrl(), URL_dashBoard);
+        login(" username_01", "");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
+    }
+    @Test
+    public void loginByUsernameH() {
+//        System.out.println("print");
+        login("Username_01", "");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
+    }
+    @Test
+    public void loginByPass() {
+//        System.out.println("print");
+        login("", "pass123");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
     }
 
-
-    @AfterMethod
-    public void testDown() {
-        driver.quit();
+    @Test
+    public void loginByPassSpecial() {
+//        System.out.println("print");
+        login("", "Pass123#");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
     }
+    @Test
+    public void loginByPassSpace() {
+        login("", " Pass123");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
+    }
+    @Test
+    public void loginByPassH() {
+        login("", "Pass123");
+        Assert.assertEquals(driver.getCurrentUrl(), URL_login);
+    }
+
 }
